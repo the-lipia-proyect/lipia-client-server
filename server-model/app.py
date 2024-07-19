4e44de79ee1cde337d87dab55802755b39f86e04
Rodrigo Franco <rodrigo.franco@codetria.com>
2024-07-18 21:35:10 -0300

    Add CMODEL_RGB implementation to server model

--- server-model/app.py
+++ server-model/app.py
@@ -25,8 +25,9 @@ def health():
 def predictions():
     try:
         payload = request.get_json()
-        model_to_execute = payload.get("model", "CHINO")
+        model_to_execute = payload.get("model", "CMODEL_RGB")
         encoded_frames = payload.get("frames", None)
+        with_rgb = payload.get("with_rgb", False)
         if encoded_frames is None:
             return (
                 jsonify({"message": "Invalid Body: the key body.frames is missing"}),
@@ -35,16 +36,18 @@ def predictions():
         # frames = base64.b64decode(encoded_frames).decode("utf-8")
         frames = encoded_frames
         # print("FRAMEs", frames)
-        model = load_model(model_to_execute)
+        model, label_dict = load_model(model_to_execute)
         if model == None:
             return (
                 jsonify({"message": "Error loading the model"}),
                 500,
             )
-        loaded_data = np.array(frames).reshape((-1, 22, 80, 112, 1))
+        loaded_data = np.array(frames).reshape(
+            (-1, 44 if with_rgb else 22, 80, 112, 3 if with_rgb else 1)
+        )
 
         prediction = model.predict(loaded_data)
-        translated_prediction = translate_prediction(prediction)
+        translated_prediction = translate_prediction(prediction, label_dict)
         return jsonify({"prediction": translated_prediction}), 200
     except Exception as e:
         print("ERROR", e)
--- server-model/utils/modelutils.py
+++ server-model/utils/modelutils.py
@@ -1,82 +1,68 @@
 import os
+from typing import Dict, Any
 
 import tensorflow as tf
 import numpy as np
 
-LABEL_DICT = {0: "boludo", 1: "chau", 2: "hola", 3: "vos", 4: "yo"}
 LIP_WIDTH = 112
 LIP_HEIGHT = 80
-def get_chino_model():
+
+
+def get_cmodel():
     model = tf.keras.models.load_model("./model/cmodel_01_07_2024.h5")
-    return model
+    return model, {0: "boludo", 1: "chau", 2: "hola", 3: "vos", 4: "yo"}
 
-def get_old_chino_model():
-    return None
-    # input_shape = (22, 80, 112, 3)
-    # model = tf.keras.Sequential(
-    #     [
-    #         tf.keras.layers.Conv3D(
-    #             8,
-    #             (3, 3, 3),
-    #             activation="relu",
-    #             input_shape=input_shape,
-    #             kernel_regularizer=tf.keras.regularizers.l2(0.001),
-    #         ),
-    #         tf.keras.layers.MaxPooling3D((2, 2, 2)),
-    #         tf.keras.layers.Conv3D(
-    #             32, (3, 3, 3), activation="relu", kernel_regularizer=tf.keras.regularizers.l2(0.001)
-    #         ),
-    #         tf.keras.layers.MaxPooling3D((2, 2, 2)),
-    #         tf.keras.layers.Conv3D(
-    #             256, (3, 3, 3), activation="relu", kernel_regularizer=tf.keras.regularizers.l2(0.001)
-    #         ),
-    #         tf.keras.layers.Flatten(),
-    #         tf.keras.layers.Dense(1024, activation="relu"),
-    #         tf.keras.layers.Dropout(0.5),
-    #         tf.keras.layers.Dense(256, activation="relu"),
-    #         tf.keras.layers.Dropout(0.5),
-    #         tf.keras.layers.Dense(64, activation="relu"),
-    #         tf.keras.layers.Dropout(0.5),
-    #         tf.keras.layers.Dense(
-    #             5, activation="softmax"
-    #         ),  # el valor 5 tiene qe coincidir con las clases que quiere adivinar
-    #     ]
-    # )
 
-    # model.load_weights(
-    #     "./model/checkpoint_5palabras_x21_v_y_h_ch_b.h5"
-    # )
-    # return model
+def get_cmodel_rgb():
+    model = tf.keras.models.load_model("./model/cmodel_p1_p2_1707_2003.h5")
+    return model, {
+        0: "adriano",
+        1: "bueno",
+        2: "chau",
+        3: "como",
+        4: "es",
+        5: "estas",
+        6: "esto",
+        7: "ezequiel",
+        8: "gabriel",
+        9: "hola",
+        10: "matias",
+        11: "prueba",
+        12: "rodrigo",
+        13: "sil",
+        14: "sos",
+        15: "soy",
+        16: "una",
+        17: "vos",
+        18: "yo",
+    }
 
-MODELS = {
-    "CHINO": get_chino_model(),
-    "OLD_CHINO": get_old_chino_model()
-}
 
-VALID_MODELS = ["CHINO","OLD_CHINO"]
-def load_model(model: str ="CHINO"):
+MODELS = {"CMODEL": get_cmodel(), "CMODEL_RGB": get_cmodel_rgb()}
+
+VALID_MODELS = ["CMODEL", "CMODEL_RGB"]
+
+
+def load_model(model: str = "CMODEL_RGB"):
     model = model.upper()
-    with tf.device('/cpu:0'):
+    with tf.device("/cpu:0"):
         if model not in VALID_MODELS:
             raise Exception(f"The model {model} does not exist")
         return MODELS[model]
 
 
-def translate_prediction(prediction):
+def translate_prediction(prediction, label_dict: Dict[str, Any]):
     prob_per_class = []
 
     maxLabel = ""
     maxProb = 0.0
 
     for i in range(len(prediction[0])):
-        prob_per_class.append((prediction[0][i], LABEL_DICT[i]))
+        prob_per_class.append((prediction[0][i], label_dict[i]))
     sorted_probs = sorted(prob_per_class, key=lambda x: x[0], reverse=True)
     for prob, label in sorted_probs:
         print(f"{label}: {prob:.3f}")
         if prob > maxProb:
             maxProb = prob
             maxLabel = label
-    return {
-        "label": maxLabel,
-        "probability": str(maxProb)
-    }
+    return {"label": maxLabel, "probability": str(maxProb)}
