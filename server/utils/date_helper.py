9155de97258ce3a693c7ccbe4814eaf4f521bf62
Rodrigo Franco <rodrigo.franco@codetria.com>
2024-09-01 21:43:41 -0300

    Fix get_utc_timestamp helper function to get utc epoch value in miliseconds as defualt

--- server/utils/date_helper.py
+++ server/utils/date_helper.py
@@ -1,5 +1,9 @@
 import datetime
 
 
-def get_utc_timestamp():
+def get_utc_timestamp_in_seconds():
     return datetime.datetime.now(datetime.UTC).timestamp()
+
+
+def get_utc_timestamp():
+    return int(datetime.datetime.now(datetime.timezone.utc).timestamp() * 1000)
