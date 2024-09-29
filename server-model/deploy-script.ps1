7aa6561bc7aed1c9fc441d543c321d5b9b2ee5e8
Rodrigo Franco <rodrigo.franco@codetria.com>
2024-09-29 18:51:40 -0300

    Fix deploy-script in server-model folder

--- server-model/deploy-script.ps1
+++ server-model/deploy-script.ps1
@@ -8,7 +8,7 @@ $DOCKERFILE = Join-Path -Path $LOCATION -ChildPath "Dockerfile"
 docker build -f $DOCKERFILE `
   -t lipiaclientserver:latest $LOCATION
 
-aws ecr get-login-password --region us-east-1 --profile default | docker login --username AWS --password-stdin 416711641372.dkr.ecr.us-east-1.amazonaws.com/anr-ia-backend-dev
+aws ecr get-login-password --region us-east-1 --profile default | docker login --username AWS --password-stdin 416711641372.dkr.ecr.us-east-1.amazonaws.com/lipia
 
 docker tag $(docker images lipiaclientserver:latest --format "{{.ID}}") 416711641372.dkr.ecr.us-east-1.amazonaws.com/lipia:latest
 
