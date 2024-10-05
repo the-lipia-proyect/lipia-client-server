41514e3d414f6c97809fb1bb5ec5bc159fc6e869
Rodrigo Franco <rodrigo.franco@codetria.com>
2024-10-05 01:25:53 -0300

    Change update user profile request dto default values

--- server/dtos/update_user_profile_request_dto.py
+++ server/dtos/update_user_profile_request_dto.py
@@ -5,8 +5,8 @@ from pydantic import BaseModel
 class UpdateUserProfileRequestDto(BaseModel):
     name: str
     surname: str
-    phone_number: Optional[str] = None
-    phone_number_emergency: Optional[str] = ""
+    phone_number: str = ""
+    phone_number_emergency: str = ""
     phone_number_doctor: Optional[str] = ""
     phone_number_doctor_emergency: Optional[str] = ""
-    blood_type: Optional[str] = ""
+    blood_type: str = ""
